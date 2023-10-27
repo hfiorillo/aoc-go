bbb9caca461f7f6bbe3bbc50568aa7b4906e7981
Harry FH <harrison.hughes@totalprocessing.com>
2023-10-27 09:09:46 +0100

    day 4 2022 part 2

--- 2022/day4/main.go
+++ 2022/day4/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"strings"
 	"time"
 
@@ -21,24 +22,32 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	part1 := 0
+
 	for _, items := range data {
-		pairs := strings.Split(items, ",")
-		first, second := strings.Split(pairs[0], "-"), strings.Split(pairs[1], "-")
-		fmt.Println(first, helpers.ToIntList(first)[0], helpers.ToIntList(first)[1])
-		fmt.Println(second, helpers.ToIntList(second)[0], helpers.ToIntList(second)[1])
-		for i := helpers.ToIntList(first)[0]; i <= helpers.ToIntList(first)[1]; i++ {
-			for j := helpers.ToIntList(second)[0]; j <= helpers.ToIntList(second)[1]; j++ {
-				fmt.Println(i, j)
-			}
+		pair := strings.Split(items, ",")
+		first, second := strings.Split(pair[0], "-"), strings.Split(pair[1], "-")
+		switch {
+		case helpers.ToIntList(first)[0] <= helpers.ToIntList(second)[0] && helpers.ToIntList(first)[1] >= helpers.ToIntList(second)[1]:
+			part1++
+		case helpers.ToIntList(first)[0] >= helpers.ToIntList(second)[0] && helpers.ToIntList(first)[1] <= helpers.ToIntList(second)[1]:
+			part1++
 		}
 	}
-	fmt.Println(time.Since(start))
-}
+	fmt.Println(part1)
 
-// Input: [... 5-79,6-6 95-95,19-95 37-94,38-94 2-12,2-36 9-46,46-47 58-80,66-81]
-// Item: strings 5-79,6-6
-// Sort into pairs, convert to integers
-// Create array of numbers
-// Is the first number in 1 before / after the first number in 2
-// Is the second number in 1 before / after the second number in 2
-// How many times do the 2 arrays ints overlap fully (1-2) (1-3)
+	part2 := 0
+	for _, items := range data {
+		pair := strings.Split(items, ",")
+		first, second := strings.Split(pair[0], "-"), strings.Split(pair[1], "-")
+		max1 := math.Max(float64(helpers.ToIntList(first)[0]), float64(helpers.ToIntList(second)[0]))
+		min2 := math.Min(float64(helpers.ToIntList(first)[1]), float64(helpers.ToIntList(second)[1]))
+		if min2 >= max1 {
+			fmt.Println(min2, max1, " | ", first, second)
+			part2++
+		}
+	}
+	fmt.Println(part2)
+	fmt.Println(start)
+}
